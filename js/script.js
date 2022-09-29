66024d43e5bcbc713e90ac125990e701a0971891
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-09-29 08:56:25 +0100

    updated branch

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// const searchBnt = document.getElementById('btn_Search');
+// const inputSearch = document.querySelector('input');
+
+// const items = [
+//     'books: assets/pix.svg',
+//     'books: assets/pix.svg',
+//     'books: assets/pix.svg',
+//     'books: assets/pix.svg',
+//     'books: assets/pix.svg'
+// ]
+// function openSearchBar(){
+//     let SearchName = inputSearch.value.toLowerCase();
+//     inputSearch.value = '';
+//     items.map((item)=>{
+//         let Splited = item.split(':')
+//         console.log(Splited)
+//         if(Splited[0].toLowerCase() === SearchName){
+//             console.log('display each book')    
+//         }
+//     })
+// }
+
+// searchBnt.addEventListener('click', openSearchBar)
